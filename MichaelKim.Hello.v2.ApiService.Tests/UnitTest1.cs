b00401fe69dd396358040db7cdc75ae38442fa9e
michaeljhkim <michaelkim291@gmail.com>
2025-11-14 17:23:14 +0000

    added note

--- MichaelKim.Hello.v2.ApiService.Tests/UnitTest1.cs
+++ MichaelKim.Hello.v2.ApiService.Tests/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿namespace MichaelKim.Hello.v2.ApiService.Tests;
 
 public class UnitTest1 {
+    // TBD: add integration tests check endpoints and connections
+
     [Fact]
     public void Test1() {
 
